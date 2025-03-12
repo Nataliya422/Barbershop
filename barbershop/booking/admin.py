d792ebd9852932388fef3625cc9a784f65c290ea
Nataliya422 <natasavkina310709@yandex.ru>
2025-03-12 10:17:02 +0700

    заполнила гитигнор, админку, requirements.txt

--- barbershop/booking/admin.py
+++ barbershop/booking/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Master, Service
+
+admin.site.register(Master)
+admin.site.register(Service)
