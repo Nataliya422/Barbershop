0751688e9f72fb94cecbdccf2711370d56b757fa
Nataliya422 <natasavkina310709@yandex.ru>
2025-03-12 11:01:25 +0700

    добавляем шаблоны для страницы

--- booking/urls.py
+++ booking/urls.py
@@ -1,7 +1,11 @@
+# booking/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('masters/', views.master_list, name='master_list'),
     path('services/', views.service_list, name='service_list'),
+    path('about/', views.about_us, name='about_us'),
+    path('book/', views.book_appointment, name='book_appointment'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,14 +1,20 @@
-
+# booking/views.py
 from django.shortcuts import render
-
-# Create your views here.
-
 from .models import Master, Service
+
+def home(request):
+    return render(request, 'home.html')
 
 def master_list(request):
     masters = Master.objects.all()
-    return render(request, 'booking/master_list.html', {'masters': masters})
+    return render(request, 'master_list.html', {'masters': masters})
 
 def service_list(request):
     services = Service.objects.all()
-    return render(request, 'booking/service_list.html', {'services': services})
+    return render(request, 'service_list.html', {'services': services})
+
+def about_us(request):
+    return render(request, 'about_us.html')
+
+def book_appointment(request):
+    return render(request, 'book_appointment.html')
