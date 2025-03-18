7cc0dbab4be22357cfb8dfdca743a495948be74b
Nataliya422 <natasavkina310709@yandex.ru>
2025-03-18 17:23:51 +0700

    настройка русификации админки, кастомизация, настройка отзывов

--- booking/admin.py
+++ booking/admin.py
@@ -1,8 +1,34 @@
+# booking/admin.py
 from django.contrib import admin
+from .models import Master, Service, Review
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
-from django.contrib import admin
-from .models import Master, Service
 
-admin.site.register(Master)
-admin.site.register(Service)
+class MasterAdmin(admin.ModelAdmin):
+    list_display = ('name', 'experience')
+    search_fields = ('name',)
+    list_filter = ('experience',)
+    list_editable = ('experience',)
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+    search_fields = ('name',)
+    list_filter = ('price',)
+    list_editable = ('price',)
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('client_name', 'master', 'rating', 'date')
+    search_fields = ('client_name', 'comment')
+    list_filter = ('rating', 'date')
+    list_editable = ('rating',)
+
+    actions = ['delete_low_rating']
+
+    def delete_low_rating(self, request, queryset):
+        queryset.filter(rating__lt=3).delete()
+
+    delete_low_rating.short_description = _("Удалить отзывы с рейтингом ниже 3")
+
+admin.site.register(Master, MasterAdmin)
+admin.site.register(Service, ServiceAdmin)
+admin.site.register(Review, ReviewAdmin)
--- booking/models.py
+++ booking/models.py
@@ -2,25 +2,37 @@
 from django.db import models
 
 class Master(models.Model):
-    name = models.CharField(max_length=100)
-    experience = models.IntegerField()
+    name = models.CharField(max_length=100, verbose_name="Имя мастера")
+    experience = models.IntegerField(verbose_name="Опыт работы")
+
+    class Meta:
+        verbose_name = "Мастер"
+        verbose_name_plural = "Мастера"
 
     def __str__(self):
         return self.name
 
 class Service(models.Model):
-    name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
+    name = models.CharField(max_length=100, verbose_name="Название услуги")
+    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Цена")
+
+    class Meta:
+        verbose_name = "Услуга"
+        verbose_name_plural = "Услуги"
 
     def __str__(self):
         return self.name
 
 class Review(models.Model):
-    master = models.ForeignKey(Master, on_delete=models.CASCADE)
-    client_name = models.CharField(max_length=100)
-    comment = models.TextField()
-    rating = models.IntegerField()  # Рейтинг от 1 до 5
-    date = models.DateTimeField(auto_now_add=True)
+    master = models.ForeignKey(Master, on_delete=models.CASCADE, verbose_name="Мастер")
+    client_name = models.CharField(max_length=100, verbose_name="Имя клиента")
+    comment = models.TextField(verbose_name="Комментарий")
+    rating = models.IntegerField(verbose_name="Рейтинг")
+    date = models.DateTimeField(auto_now_add=True, verbose_name="Дата отзыва")
+
+    class Meta:
+        verbose_name = "Отзыв"
+        verbose_name_plural = "Отзывы"
 
     def __str__(self):
-        return f"Review by {self.client_name} for {self.master.name}"
+        return f"Отзыв от {self.client_name} для {self.master.name}"
