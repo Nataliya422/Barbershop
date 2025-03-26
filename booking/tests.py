bc2a469dd611ad175ab3b3b2812ef000de345832
Nataliya422 <natasavkina310709@yandex.ru>
2025-03-26 10:51:21 +0700

    настройка отладки

--- booking/tests.py
+++ booking/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Master, Service, Review, Appointment
+from datetime import date
 
-# Create your tests here.
+class AppointmentModelTest(TestCase):
+    def setUp(self):
+        self.master = Master.objects.create(name="John Doe", experience=5)
+        self.service = Service.objects.create(name="Haircut", price=20.00)
+        Appointment.objects.create(
+            master=self.master,
+            service=self.service,
+            client_name="Client One",
+            date=date(2025, 12, 25),
+            time="14:00"
+        )
+
+    def test_appointment_details(self):
+        appointment = Appointment.objects.get(client_name="Client One")
+        self.assertEqual(appointment.date, date(2025, 12, 25))
+        self.assertEqual(appointment.time, "14:00")
